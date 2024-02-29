db3359a2ba1f0a60f6dd426cbbd4ec54d119c79f
vmakar9 <zmerin212@gmail.com>
2024-02-29 15:49:17 +0200

    Auth

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,55 @@
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+
+import { CurrentUser } from './decorators/current-user.decorator';
+import { SkipAuth } from './decorators/skip-auth.decorator';
+
+import { AuthUserResponseDto } from './dto/response/auth-user.response.dto';
+import { TokenResponseDto } from './dto/response/token.response.dto';
+import { JwtRefreshGuard } from './guards/jwt-refresh.guard';
+import { IUserData } from './interfaces/user-data.interface';
+import { AuthService } from './services/auth.service';
+import {SignUpRequestDto} from "./dto/request/siqn-up.request.dto";
+import {SignInRequestDto} from "./dto/request/siqn-in.request.dto";
+
+@ApiTags('Auth')
+@Controller({ path: 'auth', version: '1' })
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @SkipAuth()
+    @ApiOperation({ summary: 'Registration' })
+    @Post('sign-up')
+    public async signUp(
+        @Body() dto: SignUpRequestDto,
+    ): Promise<AuthUserResponseDto> {
+        return await this.authService.signUp(dto);
+    }
+
+    @SkipAuth()
+    @ApiOperation({ summary: 'Login' })
+    @Post('sign-in')
+    public async signIn(
+        @Body() dto: SignInRequestDto,
+    ): Promise<AuthUserResponseDto> {
+        return await this.authService.signIn(dto);
+    }
+
+    @ApiBearerAuth()
+    @ApiOperation({ summary: 'Logout' })
+    @Post('logout')
+    public async logout(@CurrentUser() userData: IUserData): Promise<void> {
+        await this.authService.logout(userData);
+    }
+
+    @SkipAuth()
+    @ApiBearerAuth()
+    @UseGuards(JwtRefreshGuard)
+    @ApiOperation({ summary: 'Update token pair' })
+    @Post('refresh')
+    public async updateRefreshToken(
+        @CurrentUser() userData: IUserData,
+    ): Promise<TokenResponseDto> {
+        return await this.authService.refreshToken(userData);
+    }
+}
